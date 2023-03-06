2019173938d0bc187070b4f53e1f4faae1c0a96e
AndyCotruta <acotruta18@gmail.com>
2023-03-06 17:40:10 +0100

    added google passport

--- src/server.js
+++ src/server.js
@@ -2,8 +2,8 @@ import express from "express";
 import listEndpoints from "express-list-endpoints";
 import mongoose from "mongoose";
 import cors from "cors";
+import passport from "passport";
 import restaurantRouter from "./restaurants/index.js";
-import createHttpError from "http-errors";
 import bodyParser from "body-parser";
 import {
   badRequestHandler,
@@ -16,15 +16,18 @@ import categoriesRouter from "./categories/index.js";
 import featuredCategoriesRouter from "./featuredCategories/index.js";
 import usersRouter from "./users/index.js";
 import filesRouter from "./files/index.js";
+import googleStrategy from "./lib/auth/google.js";
 
 const server = express();
 const port = process.env.PORT;
+
+passport.use("google", googleStrategy);
 
 server.use(cors());
 
 server.use(bodyParser.json({ limit: "50mb" }));
 server.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
-
+server.use(passport.initialize());
 server.use(express.json());
 
 server.use("/restaurants", restaurantRouter);
