4475dd9d3a95f211e37af3ff9126383fd21ef49a
AndyCotruta <acotruta18@gmail.com>
2023-03-06 13:29:51 +0100

    added image upload for post and put (restaurants and dishes)

--- src/dishes/index.js
+++ src/dishes/index.js
@@ -1,30 +1,47 @@
 import express from "express";
 import httpErrors from "http-errors";
+import multer from "multer";
+import { v2 as cloudinary } from "cloudinary";
+import { CloudinaryStorage } from "multer-storage-cloudinary";
 import RestaurantModel from "../restaurants/model.js";
+
+const cloudinaryDishes = multer({
+  storage: new CloudinaryStorage({
+    cloudinary, // cloudinary is going to search in .env vars for smt called process.env.CLOUDINARY_URL
+    params: {
+      folder: "bamboobites/dishes",
+    },
+  }),
+}).single("dishImage");
 
 const dishesRouter = express.Router();
 
 const { NotFound, Unauthorized, BadRequest } = httpErrors;
 
-dishesRouter.post("/:restaurantId/dishes", async (req, res, next) => {
-  try {
-    const restaurantId = req.params.restaurantId;
-    const newDish = req.body;
-    const updatedRestaurant = await RestaurantModel.findByIdAndUpdate(
-      restaurantId,
-      { $push: { dishes: newDish } },
-      { new: true, runValidators: true, timestamps: true }
-    );
-    if (updatedRestaurant) {
-      res.status(201).send(updatedRestaurant);
-    } else {
-      next(NotFound(`Restaurant with id ${restaurantId} not found`));
+dishesRouter.post(
+  "/:restaurantId/dishes",
+  cloudinaryDishes,
+  async (req, res, next) => {
+    try {
+      const url = req.file.path;
+      const restaurantId = req.params.restaurantId;
+      const newDish = { ...req.body, image: url };
+      const updatedRestaurant = await RestaurantModel.findByIdAndUpdate(
+        restaurantId,
+        { $push: { dishes: newDish } },
+        { new: true, runValidators: true, timestamps: true }
+      );
+      if (updatedRestaurant) {
+        res.status(201).send(updatedRestaurant);
+      } else {
+        next(NotFound(`Restaurant with id ${restaurantId} not found`));
+      }
+    } catch (error) {
+      console.log(error);
+      next(error);
     }
-  } catch (error) {
-    console.log(error);
-    next(error);
   }
-});
+);
 
 dishesRouter.get("/:restaurantId/dishes", async (req, res, next) => {
   try {
@@ -64,33 +81,39 @@ dishesRouter.get("/:restaurantId/dishes/:dishId", async (req, res, next) => {
   }
 });
 
-dishesRouter.put("/:restaurantId/dishes/:dishId", async (req, res, next) => {
-  try {
-    const restaurantId = req.params.restaurantId;
-    const dishId = req.params.dishId;
-    const restaurant = await RestaurantModel.findById(restaurantId);
-    if (restaurant) {
-      const index = restaurant.dishes.findIndex(
-        (dish) => dish._id.toString() === dishId
-      );
-      if (index !== -1) {
-        restaurant.dishes[index] = {
-          ...restaurant.dishes[index].toObject(),
-          ...req.body,
-        };
-        await restaurant.save();
-        res.status(201).send(restaurant);
+dishesRouter.put(
+  "/:restaurantId/dishes/:dishId",
+  cloudinaryDishes,
+  async (req, res, next) => {
+    try {
+      const url = req.file.path;
+      const restaurantId = req.params.restaurantId;
+      const dishId = req.params.dishId;
+      const restaurant = await RestaurantModel.findById(restaurantId);
+      if (restaurant) {
+        const index = restaurant.dishes.findIndex(
+          (dish) => dish._id.toString() === dishId
+        );
+        if (index !== -1) {
+          restaurant.dishes[index] = {
+            ...restaurant.dishes[index].toObject(),
+            ...req.body,
+            image: url,
+          };
+          await restaurant.save();
+          res.status(201).send(restaurant);
+        } else {
+          next(NotFound(`Dish with id ${dishId} was not found`));
+        }
       } else {
-        next(NotFound(`Dish with id ${dishId} was not found`));
+        next(NotFound(`Restaurant with id ${restaurantId} was not found`));
       }
-    } else {
-      next(NotFound(`Restaurant with id ${restaurantId} was not found`));
+    } catch (error) {
+      console.log(error);
+      next(error);
     }
-  } catch (error) {
-    console.log(error);
-    next(error);
   }
-});
+);
 
 dishesRouter.delete("/:restaurantId/dishes/:dishId", async (req, res, next) => {
   try {
--- src/restaurants/index.js
+++ src/restaurants/index.js
@@ -1,15 +1,27 @@
 import express, { request } from "express";
 import httpErrors from "http-errors";
-
+import multer from "multer";
+import { v2 as cloudinary } from "cloudinary";
+import { CloudinaryStorage } from "multer-storage-cloudinary";
 import RestaurantModel from "./model.js";
+
+const cloudinaryRestaurant = multer({
+  storage: new CloudinaryStorage({
+    cloudinary,
+    params: {
+      folder: "bamboobites/restaurants",
+    },
+  }),
+}).single("restaurantImage");
 
 const restaurantRouter = express.Router();
 
 const { NotFound, Unauthorized, BadRequest } = httpErrors;
 
-restaurantRouter.post("/", async (req, res, next) => {
+restaurantRouter.post("/", cloudinaryRestaurant, async (req, res, next) => {
   try {
-    const newRestaurant = new RestaurantModel(req.body);
+    const url = req.file.path;
+    const newRestaurant = new RestaurantModel({ ...req.body, image: url });
     const { _id } = await newRestaurant.save();
     res.status(201).send(`Restaurant with id ${_id} was created successfully`);
   } catch (error) {
@@ -42,24 +54,29 @@ restaurantRouter.get("/:restaurantId", async (req, res, next) => {
     next(error);
   }
 });
-restaurantRouter.put("/:restaurantId", async (req, res, next) => {
-  try {
-    const restaurantId = req.params.restaurantId;
-    const updatedRestaurant = await RestaurantModel.findByIdAndUpdate(
-      restaurantId,
-      req.body,
-      { new: true, runValidators: true }
-    );
-    if (updatedRestaurant) {
-      res.status(201).send(updatedRestaurant);
-    } else {
-      next(NotFound(`Restaurant with id ${restaurantId} was not found`));
+restaurantRouter.put(
+  "/:restaurantId",
+  cloudinaryRestaurant,
+  async (req, res, next) => {
+    try {
+      const url = req.file.path;
+      const restaurantId = req.params.restaurantId;
+      const updatedRestaurant = await RestaurantModel.findByIdAndUpdate(
+        restaurantId,
+        { ...req.body, image: url },
+        { new: true, runValidators: true }
+      );
+      if (updatedRestaurant) {
+        res.status(201).send(updatedRestaurant);
+      } else {
+        next(NotFound(`Restaurant with id ${restaurantId} was not found`));
+      }
+    } catch (error) {
+      console.log(error);
+      next(error);
     }
-  } catch (error) {
-    console.log(error);
-    next(error);
   }
-});
+);
 restaurantRouter.delete("/:restaurantId", async (req, res, next) => {
   try {
     const restaurantId = req.params.restaurantId;
