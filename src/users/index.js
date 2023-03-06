3291ce5f95d0f9e9b78677afec21f8e584946c2b
AndyCotruta <acotruta18@gmail.com>
2023-03-06 20:11:57 +0100

    lets try with url

--- src/users/index.js
+++ src/users/index.js
@@ -76,7 +76,7 @@ usersRouter.get(
   passport.authenticate("google", { session: false }),
   async (req, res, next) => {
     console.log(req.user);
-    res.send(req.user.accessToken);
+    res.redirect(`${process.env.FE_URL}?accessToken=${req.user.accessToken}`);
   }
 );
 
