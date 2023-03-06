21ef8c68bf4f3b51dc7fd729bf3f5ca52d593cda
AndyCotruta <acotruta18@gmail.com>
2023-03-06 15:46:06 +0100

    category for restaurants is an array

--- src/restaurants/model.js
+++ src/restaurants/model.js
@@ -23,7 +23,7 @@ const restaurantSchema = new Schema(
     short_description: { type: String, required: true },
     image: { type: String, required: true },
     address: { type: String, required: true },
-    category: { type: String, required: true },
+    category: [{ type: String, required: true }],
     rating: { type: Number, max: 5, default: 1, required: true },
     lat: { type: Number, required: true },
     lon: { type: Number, required: true },
